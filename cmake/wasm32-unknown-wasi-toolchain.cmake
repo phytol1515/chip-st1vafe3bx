4cec455f7d27b208c4270b3c008910d51415c1b6
Pekka Nikander <pekka.nikander@iki.fi>
2025-02-03 13:40:25 +0200

    feat: Add initial support for Github actions

--- cmake/wasm32-unknown-wasi-toolchain.cmake
+++ cmake/wasm32-unknown-wasi-toolchain.cmake
@@ -5,10 +5,10 @@ cmake_minimum_required(VERSION 3.13)
 # ---------------------------------------------------------------------------
 
 # The installation location of your WASI sysroot
-set(WASI_SYSROOT /opt/wasi-sdk/share/sysroot/ CACHE PATH "Location of WASI sysroot.")
+set(WASI_SYSROOT /opt/wasi-sysroot CACHE PATH "Location of WASI sysroot.")
 
-# Path to a custom LLVM installation (comment out or adjust if using host clang).
-set(CUSTOM_LLVM_PATH /usr/bin CACHE PATH "Path to custom LLVM installation")
+# Path to a LLVM installation (comment out or adjust if using host clang).
+set(CLANG_PATH /usr/bin CACHE PATH "Path to LLVM installation")
 
 # ---------------------------------------------------------------------------
 # Tell CMake we’re cross-compiling for a generic system. Without this,
@@ -18,10 +18,10 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_CROSSCOMPILING TRUE)
 
 # ---------------------------------------------------------------------------
-# Use custom LLVM, as e.g. macOS clang cannot produce WASM
+# Use a specific version LLVM, as e.g. macOS clang cannot produce WASM
 # ---------------------------------------------------------------------------
-find_program(CMAKE_C_COMPILER   NAMES clang   HINTS ${CUSTOM_LLVM_PATH})
-find_program(CMAKE_CXX_COMPILER NAMES clang++ HINTS ${CUSTOM_LLVM_PATH})
+find_program(CMAKE_C_COMPILER   NAMES clang   HINTS ${CLANG_PATH})
+find_program(CMAKE_CXX_COMPILER NAMES clang++ HINTS ${CLANG_PATH})
 
 # ---------------------------------------------------------------------------
 # Compiler and Linker Options for WASM32
